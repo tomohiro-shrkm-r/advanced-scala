bad063fbbd95761235a0e7796234d88ebaff4ee4
tomo.shirakami <tomohiro.shirakami_r@outlook.jp>
2020-08-12 09:48:20 +0900

    test commit

--- src/playground/JavaPlayground.java
+++ src/playground/JavaPlayground.java
@@ -5,6 +5,6 @@ package playground;
  */
 public class JavaPlayground {
     public static void main(String[] args) {
-        System.out.println("Hello, Java");
+        System.out.println("Hello, Shira!");
     }
 }
