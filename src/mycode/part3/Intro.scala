a5d786aba60aab3749825e792b654fe3763f9daf
tomo.shirakami <tomohiro.shirakami_r@outlook.jp>
2020-08-12 22:33:56 +0900

    threadの扱いで難しい点・面倒な点の解説

--- src/mycode/part3/Intro.scala
+++ src/mycode/part3/Intro.scala
@@ -43,4 +43,53 @@ object Intro extends App {
 //  pool.execute(() => println("should not appear")) // this throws an exeption in the calling thread.
 
 //  pool.shutdownNow()  // this leads exception in println.
+
+//  def runInParallel = {
+//    var x       = 0
+//    val thread1 = new Thread(() => x = 1)
+//    val thread2 = new Thread(() => x = 2)
+//
+//    thread1.start()
+//    thread2.start()
+//    println(x)
+//  }
+
+  // race condition
+//  for (i <- 1 to 10000) runInParallel
+
+  // 改行
+  println()
+
+  class BankAccount(var amount: Int) {
+    override def toString: String = s"$amount"
+  }
+
+  def buy(account: BankAccount, thing: String, price: Int) = {
+    account.amount -= price
+    println(s"I have bought $thing")
+    println(s"my account is now $account")
+  }
+
+  for (i <- 1 to 10000) {
+    val account = new BankAccount(50000)
+//    val t1      = new Thread(() => buy(account, "shoes", 3000))
+//    val t2      = new Thread(() => buy(account, "iPhone", 4000))
+    val t1 = new Thread(() => buySafe(account, "shoes", 3000))
+    val t2 = new Thread(() => buySafe(account, "iPhone", 4000))
+
+    t1.start()
+    t2.start()
+    Thread.sleep(100)
+    println()
+  }
+
+  // option1 : use synchronized()
+  def buySafe(account: BankAccount, thing: String, price: Int): Unit = {
+    account.synchronized {
+      // no two threads can evaluate this at the same time
+      account.amount -= price
+      println(s"I have bought $thing")
+      println(s"my account is now $account")
+    }
+  }
 }
